211627327f6f5766ab245aa954a04cd0e3a6f447
Emilio Salazar Cardozo <esalazar@axon.ece.cmu.edu>
2020-06-22 19:15:36 -0400

    Add FilteredSpikeSet part table and ways to load/save them and BinnedSpikeSets

--- setup/DataJointSetup.py
+++ setup/DataJointSetup.py
@@ -121,7 +121,9 @@ class BinnedSpikeSetProcessParams(dj.Manual):
     bss_params_id : int auto_increment # params id
     ---
     start_offset : int # offset in ms (from start_time_alignment in the specific params)
+    start_offset_from_location : enum('stateStart', 'stateEnd') # location to offset from in start_alignment_state
     end_offset : int # offset in ms (from end_time_alignment in the specific params)
+    end_offset_from_location : enum('stateStart', 'stateEnd') # location to offset from in end_alignment_state
     bin_size : int # bin size in ms
     firing_rate_thresh : float # lowest firing rate threshold per channel
     fano_factor_thresh : float # highest fano factor threshold per channel
@@ -151,6 +153,124 @@ class BinnedSpikeSetInfo(dj.Manual):
     end_time : blob # true end_time in ms of each trisl -- end_time_alignment + end_offset
     """
 
+    class FilteredSpikeSetInfo(dj.Part):
+        definition = """
+        # for filtered spike sets
+        -> BinnedSpikeSetInfo
+        fss_rel_path_from_parent: varchar(100) # this is the directory within above's relative path where this set lives
+        fss_param_hash : char(32) # for looking this entry up
+        ---
+        fss_hash : char(32) # for the data integrity
+        trial_filter = null : blob # filter of the trial on the binned spike set info
+        trial_num : int # number of trials; useful for filtering shuffles
+        ch_filter = null : blob # filter of the channels on the binned spike set info
+        ch_num : int # number of channels; useful for filtering shuffles
+        filter_reason : enum('shuffle', 'other')
+        filter_description = null : varchar(100) # reason for filter (if not shuffle, usually)
+        """
+
+    def filterBSS(self, filterReason, filterDescription, binnedSpikeSet = None, trialFilter = None, channelFilter = None):
+        keys = self.fetch("KEY")
+        if len(keys)>1:
+            raise Exception("Not sure that you want to filter multiple spike sets with the same parameters...")
+
+
+        if binnedSpikeSet is None:
+            # if you forgot to include the binnedSpikeSet...
+            #
+            # or you have voodoo magic knowing about number of
+            # trials/channels... possible? Might allow you to group things if I
+            # decide to allow this function to be called for groups of
+            # lists of binnedSpikeSets
+            bss = self.grabBinnedSpikes()
+        else:
+            bss = binnedSpikeSet
+
+        if len(bss)>1:
+            raise Exception("filterBSS should only be called on one binned spike set...")
+        elif len(bss) == 0:
+            raise Exception("filterBSS didn't find any binned spike sets")
+        else:
+            bssFiltered = bss[0]
+            key = keys[0]
+
+            bssFilterParams = dict(filter_reason = filterReason, filter_description = filterDescription)
+            if trialFilter is not None:
+                bssFiltered = bssFiltered[trialFilter]
+                bssFilterParams.update(dict(trial_filter = trialFilter))
+
+            if channelFilter is not None:
+                bssFiltered = bssFiltered[:, channelFilter]
+                bssFilterParams.update(dict(ch_filter = channelFilter))
+
+            bssFilterParams.update(dict(
+                    trial_num = bssFiltered.shape[0],
+                    ch_num = bssFiltered.shape[1]
+                ))
+
+
+            defaultParams = loadDefaultParams()
+            dataPath = Path(defaultParams['dataPath'])
+
+            bssPath = Path(key['bss_relative_path']).parent # because it includes the .dill file
+            filteredSpikeSetDill = 'filteredSpikeSet.dill'
+
+            # a nice way to distinguish the path for each filter based on extraction parameters...
+            bSSFilteredProcParamsJson = json.dumps(str(bssFilterParams), sort_keys=True) # needed for consistency as dicts aren't ordered
+            bSSFilteredProcParamsHash = hashlib.md5(bSSFilteredProcParamsJson.encode('ascii')).hexdigest()
+
+            pathRelativeToParent = Path('filteredSpikes_%s' % bSSFilteredProcParamsHash[:5]) / filteredSpikeSetDill
+            fullPath = dataPath / bssPath / pathRelativeToParent
+
+            fsi = self.FilteredSpikeSetInfo()
+            existingSS = fsi[{k : v for k,v in bssFilterParams.items() if k in fsi.primary_key}] 
+            if len(existingSS) > 1:
+                raise Exception('Multiple filtered spike sessions have been saved with these parameters')
+            elif len(existingSS) > 0:
+                if not fullPath.exists():
+                    print('Db record existed for FilteredSpikeSet but not file... saving file now')
+                    fullPath.parent.mkdir(parents=True, exist_ok = True)
+                    with fullPath.open(mode='wb') as filteredSpikeSetDillFh:
+                        pickle.dumps(bssFiltered, filteredSpikeSetDillFh)
+#                with fullPath.open(mode='rb') as filteredSpikeSetDillFh:
+#                    bssFiltered = pickle.load(filteredSpikeSetDillFh)
+            else:
+                fullPath.parent.mkdir(parents=True, exist_ok = True)
+                with fullPath.open(mode='wb') as filteredSpikeSetDillFh:
+                    pickle.dump(bssFiltered, filteredSpikeSetDillFh)
+
+                bssFilteredHash = hashlib.md5(str(bssFiltered).encode('ascii')).hexdigest()
+
+                bssFilterInfoVals = dict(**bssFilterParams,
+                        fss_param_hash = bSSFilteredProcParamsHash,
+                        fss_rel_path_from_parent = str(pathRelativeToParent),
+                        fss_hash = bssFilteredHash
+                    )
+
+                fsi.insert1(dict(
+                        **key,
+                        **bssFilterInfoVals
+                    ))
+        return bssFiltered
+
+    # for filtering spike sets... note that only filterParams
+    def grabBinnedSpikes(self, returnPath = False):
+        defaultParams = loadDefaultParams()
+        dataPath = Path(defaultParams['dataPath'])
+
+        binnedSpikeSets = []
+
+        # give some ordering here
+        bssPaths = self.fetch('bss_relative_path')
+        for path in bssPaths:
+            fullPath = dataPath / path
+            with fullPath.open(mode='rb') as binnedSpikeSetDillFh:
+                bss = pickle.load(binnedSpikeSetDillFh)
+                binnedSpikeSets.append(bss)
+
+        if returnPath:
+            return binnedSpikeSets, bssPaths
+        return binnedSpikeSets
 
 if __name__ == '__main__':
     ds = DatasetInfo()
