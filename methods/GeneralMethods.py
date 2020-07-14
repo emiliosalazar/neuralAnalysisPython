304479181058c0a000c146a676ce9f0e68285552
Emilio Salazar Cardozo <esalazar@axon.ece.cmu.edu>
2020-07-14 12:05:33 -0400

    Utility for getting user confirmation--i.e. for confirmation prior to deleting files

--- methods/GeneralMethods.py
+++ methods/GeneralMethods.py
@@ -15,6 +15,27 @@ def loadDefaultParams(defParamName = "paramDefs.txt", defParamBase=None):
 
     return json.load(defParamFile.open())
 
+# Shamelessley ripped from DataJoint, but I'd rather have it here because it
+# seems more like an internal DataJoint util not meant to be used by other
+# libraries directly
+def userChoice(prompt, info, choices=("yes", "no"), default=None):
+    """
+    Prompts the user for confirmation.  The default value, if any, is capitalized.
+    :param prompt: Information to display to the user.
+    :param choices: an iterable of possible choices.
+    :param default: default choice
+    :return: the user's choice
+    """
+    assert default is None or default in choices
+    choice_list = ', '.join((choice.title() if choice == default else choice for choice in choices))
+    response = None
+    while response not in choices:
+        print(info)
+        response = input(prompt + ' [' + choice_list + ']: ')
+        response = response.lower() if response else default
+    return response
+
+
 def prepareMatlab(eng=None):
     # print('HI')
     # from importlib import reload
