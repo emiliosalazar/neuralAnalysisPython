fafeca221e00f873826d08df2923894030fc5b4a
Emilio Salazar Cardozo <esalazar@axon.ece.cmu.edu>
2020-07-23 20:49:59 -0400

    First steps to full fledge db storage of analysis runs...

--- decorators/AnalysisCallDecorators.py
+++ decorators/AnalysisCallDecorators.py
@@ -6,12 +6,20 @@ Created on Wed Apr 15 11:12:18 2020
 @author: emilio
 """
 import functools
-from setup.DataJointSetup import AnalysisRunInfo
 import inspect
 import sh
+from datetime import datetime
+
+from setup.DataJointSetup import AnalysisRunInfo
 from methods.GeneralMethods import loadDefaultParams
 
+
+
 def saveCallsToDatabase(func):
+
+    defaultParams = loadDefaultParams()
+    rootPath = defaultParams['rootCodePath']
+
     @functools.wraps(func)
     def saveCallToDb(*args, **kwargs):
         ari = AnalysisRunInfo()
@@ -19,23 +27,30 @@ def saveCallsToDatabase(func):
         analysisMethod = func.__name__
         callSignature = inspect.signature(func)
         methodInputs = inspect.getcallargs(func, *args, **kwargs)
-        funcRet = func(*args, **kwargs)
 
-        gitRepo = sh.git('--no-pager', _cwd = '../') # a level up from decorators is now baked in ... >.>
+        runStartTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        funcRet = func(*args, **kwargs)
+        runEndTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        gitRepo = sh.git.bake('--no-pager', _cwd = rootPath) # a level up from decorators is now baked in ... >.>
         diffRepo = gitRepo.diff('--no-color')
         gitHash = gitRepo.log("--pretty=format:%H", "-n 1")
 
-        outputFiles = funcRet['outputFiles']
-        ari.insert1(dict(
-            analysis_method = analysisMethod,
-            method_signature = str(callSignature),
-            git_commit = gitHash,
-            patch = diffRepo,
-            method_inputs = funcRet,
-            date = ,
-            output_files = outputFiles,
-            metadata = ,
-        ))
+        if funcRet is not None:
+            outputFiles = funcRet['outputFiles'] if 'outputFiles' in funcRet else None
+        else:
+            outputFiles = None
+#        ari.insert1(dict(
+#            analysis_method = analysisMethod,
+#            method_call_signature = str(analysisMethod) + str(callSignature),
+#            git_commit = gitHash,
+#            patch = diffRepo,
+#            method_inputs = methodInputs,
+#            date_start = runStartTime,
+#            date_end = runEndTime,
+#            output_files = outputFiles,
+#            metadata = ,
+#        ))
         return funcRet
     
     return saveCallToDb
--- setup/DataJointSetup.py
+++ setup/DataJointSetup.py
@@ -1422,17 +1422,17 @@ class GpfaAnalysisInfo(dj.Manual):
         super().drop()
 
 
-class AnalysisRun: #(dj.Manual): NOTE uncomment inheritence!
+class AnalysisRunInfo: #(dj.Manual): NOTE uncomment inheritence!
     definition = """
     # analysis file/run info
-    analysis_file : varchar(100) # name of analysis file run
-    git_commit : char(32) # don't remember how long a has a git commit has
-    patch : varchar(50000) # this is  super long... but it's to keep the patch information so the analysis can be recreated
-    -> BinnedSpikeSetInfo
+    analysis_method : varchar(100) # name of analysis file run
+    method_call_signature : varchar(500) # calling signature for the method
+    git_commit : char(32) # don't remember how long a hash a git commit has
     ---
-    # in case some filtered version of the above binned spike set was used...
-    -> [nullable] BinnedSpikeSetInfo
-    date : datenum # date analysis was run
+    patch : varchar(500000) # this is  super long... but it's to keep the patch information so the analysis can be recreated
+    method_inputs : blob # the inputs to this call--since all calls are different, this is a blob!
+    date_start : datenum # date analysis started running
+    date_end : datenum # date analysis finished running
     output_files : blob # this is gonna contain output files (especially figures!) for this analysis
     metadata : blob # this has metadata that would likely be stored by the patch as well, but in case its easier to grab from here...
     """
