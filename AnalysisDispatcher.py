50befd517cc00f67290542e115270a4fe1ac6b45
Emilio Salazar Cardozo <esalazar@axon.ece.cmu.edu>
2020-09-15 10:25:07 -0400

    Deprecating AnalysisDispatcher--it lived a short but full life

--- AnalysisDispatcher.py
+++ AnalysisDispatcher.py
@@ -5,12 +5,14 @@ they're called from the top level
 import sys
 import getopt
 
+# NOTE DEPRECATED: I'm commenting out the imports so it errors in case we ever
+# do use it and then I can go back and make sure it's up to scruff
 # we're importing all valid analyses here; yeah, it's an extra step, but it's
 # unavoidable really, and in some sense lets me be intentional about adding new
 # analyses
-from analyses.CrossareaMatchedCovarianceComparison import crossareaMatchedCovarianceComparison
-from analyses.V4Stim1sDimensionality import v4Stim1sDimensionality
-from analyses.SweepOfExtractionParams import sweepOfExtractionParams
+#from analyses.CrossareaMatchedCovarianceComparison_vAct import crossareaMatchedCovarianceComparison
+#from analyses.V4Stim1sDimensionality import v4Stim1sDimensionality
+#from analyses.SweepOfExtractionParams import sweepOfExtractionParams
 
 def analysisDispatcher(func, *funcArgs, **funcKeyArgs):
     func(funcArgs, funcKeyArgs)
