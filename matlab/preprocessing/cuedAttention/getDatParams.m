d6de85f3bd7245b691683eda29ae20e5b37edc71
Emilio Salazar Cardozo <esalazar@glia.ece.local.cmu.edu>
2021-01-29 21:43:59 -0500

    Clear params for every trial when grabbing from NEV

--- matlab/preprocessing/cuedAttention/getDatParams.m
+++ matlab/preprocessing/cuedAttention/getDatParams.m
@@ -1,21 +1,22 @@
 function dat = getDatParams(dat)
     for n = 1:length(dat)
-    preTrial{n}  = dat(n).text;
-    variables = regexp(preTrial{n},';','split');
-    for j = 1:length(variables)
-        k = strfind(variables{j},'=');
-        if k            
-            lhs = variables{j}(1:k-1);
-            rhs = variables{j}(k+1:end);
-            % MATT - should we check here to see if any value has
-            % changed and report back if it has?
-            try
-                eval(['params.' lhs '=[' rhs '];']);
-            catch
-                eval(['params.' lhs '=''' rhs ''';']);
+        clear params % gotta clear this variable so history doesn't get retained when it gets eval-ed below
+        preTrial{n}  = dat(n).text;
+        variables = regexp(preTrial{n},';','split');
+        for j = 1:length(variables)
+            k = strfind(variables{j},'=');
+            if k            
+                lhs = variables{j}(1:k-1);
+                rhs = variables{j}(k+1:end);
+                % MATT - should we check here to see if any value has
+                % changed and report back if it has?
+                try
+                    eval(['params.' lhs '=[' rhs '];']);
+                catch
+                    eval(['params.' lhs '=''' rhs ''';']);
+                end
             end
         end
-    end
-    dat(n).params.trial = params;
+        dat(n).params.trial = params;
     end
 end
